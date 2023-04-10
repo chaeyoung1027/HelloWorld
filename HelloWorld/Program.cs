b5ff56e4e18a87a2c82fd5518a6a21a619edf89d
Chaeyoung Lim <rnldyal1027@naver.com>
2023-04-10 12:08:34 +0900

    for문

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,6 +7,13 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            foreach (var item in args)
+            {
+
+            }
+            for (var i = 0; i < 10; i++) {
+            }
         }
     }
 }
